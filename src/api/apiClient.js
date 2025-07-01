89596cd47d4a7c7ce8488752b73ff81977b41a34
felix alegria <felix.alegria@bjungle.net>
2025-06-30 23:16:38 -0500

    add build

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://backend-decameron-app-3.onrender.com:8000/api/v1',
   //timeout: 10000, // Tiempo máximo de espera de 10 segundos
   headers: {
     'Content-Type': 'application/json',
